440840d9e40f0441fbae4e62e03ebb6dcfcf2c29
Bruno Tassinari <bruno.fagundes80@gmail.com>
2024-06-23 00:48:03 -0300

    refactor: renew project

---
+++ CleanArchDotnet.WebIU/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanArchDotnet.WebIU.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
