74973deb2750065cab106d04c0446552153512a2
Bruno Tassinari <bruno.tassinari@kbase.com.br>
2024-06-26 21:28:59 -0300

    feat: db context

---
+++ CleanArchDotnet.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchDotnet.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchDotnet.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
